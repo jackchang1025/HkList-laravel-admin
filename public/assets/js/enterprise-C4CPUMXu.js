c282225c1055ef3d3d08d73ebb909d2a5e8ecbd8
weijiajia <shadowmatthew1025@gamil.com>
2024-12-11 21:10:59 +0800

    refactor(public): 更新 HTML 结构并添加新资源
    - 更新 index.html 中的 script 和 link 标签
    - 添加新的 CSS 和 JS 文件
    - 修改现有文件的引用路径

---
+++ public/assets/js/enterprise-C4CPUMXu.js
@@ -0,0 +1,1 @@
+import{i as t}from"./index-B0cHC-7S.js";const n=e=>t.get("/admin/enterprise",{params:e}),i=e=>t.post("/admin/enterprise",e),a=(e,s)=>t.patch(`/admin/enterprise/${e}`,s),p=e=>t.delete(`/admin/enterprise/${e}`),d=e=>t.delete("/admin/enterprise",{data:{ids:e}}),c=e=>t.patch(`/admin/enterprise/${e}/toggle`);export{d as a,i as c,p as d,n as g,c as t,a as u};
